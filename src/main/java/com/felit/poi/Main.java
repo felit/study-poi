9815a25616bd5edfc665e553af2ba7ca88b5bab0
felit <nofelit@126.com>
2014-11-25 17:32:36 +0800

    init

---
+++ src/main/java/com/felit/poi/Main.java
@@ -0,0 +1,7 @@
+package com.felit.poi;
+
+/**
+ *
+ */
+public class Main {
+}
---
+++ src/test/java/com/felit/poi/TestExcel.java
@@ -0,0 +1,14 @@
+package com.felit.poi;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ */
+@Test
+public class TestExcel {
+    @Test
+    public void testCreateExcel() {
+
+    }
+}
